b414a94dd36df5528402a9c5441dd2d9ad76c34d
VinnyDevCoder <vinicius96144@gmail.com>
2023-10-28 22:43:25 -0300

    adicionado a model de item

---
+++ src/main/java/web/model/Item.java
@@ -0,0 +1,80 @@
+package web.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Item {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private int qtd;
+	
+	@NotNull
+	private String tipo;
+	
+	
+	private boolean status;
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getQtd() {
+		return qtd;
+	}
+
+
+	public void setQtd(int qtd) {
+		this.qtd = qtd;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public boolean isStatus() {
+		return status;
+	}
+
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	
+
+}
